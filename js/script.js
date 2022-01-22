eb9fedb60460f692e1efbf0d00d18a260671cf64
QWERTY <77407536+stechwer@users.noreply.github.com>
2022-01-22 09:13:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var Mixer = mixitup ( '.menu-wpapper' );
+    $('.galerie-wrapper').slick({
+        arrows:false,
+        autoplay:3000,
+        dots:true
+    });
+  });
